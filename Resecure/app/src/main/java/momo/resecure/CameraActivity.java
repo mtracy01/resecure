69d1e6dcce4901b32ad2a9e9f5700127193c2a85
XJi <xiaojingji5@gmail.com>
2016-09-24 23:50:23 -0400

    Open Camera and Record Video

--- Resecure/app/src/main/java/momo/resecure/CameraActivity.java
+++ Resecure/app/src/main/java/momo/resecure/CameraActivity.java
@@ -1,21 +1,96 @@
 package momo.resecure;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
+    private static final int CAMERA_CAPTUE_VIDEO_REQUEST_CODE = 1;
+    public static String serialNum;
+    public VideoView videoPreview;
+    private Uri fileUri;
+    public static File mediaFile;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2);
+        
+        serialNum = Build.SERIAL; //Get the serial number of the device
+        Log.d("SERIALID",serialNum);
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_CAPTUE_VIDEO_REQUEST_CODE);
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File f = new File(fileUri.getPath());
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            sendBroadcast(mediaScanIntent);
+        }
     }
-
+    //Stop video stream when clicking Stop button
     public void StopVideo(View view) {
-        //Start recording the video and sending stream to the server
+
     }
 
+    public void onActivityResult(int request){
 
+    }
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
 
 }
--- Resecure/app/src/main/java/momo/resecure/LoginActivity.java
+++ Resecure/app/src/main/java/momo/resecure/LoginActivity.java
@@ -3,6 +3,7 @@ package momo.resecure;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -86,12 +87,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                //attemptLogin();
+                switchToCameraActivity();
             }
         });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void switchToCameraActivity() {
+        Intent switchToCameraActivity = new Intent(this, CameraActivity.class);
+        startActivity(switchToCameraActivity);
     }
 
     private void populateAutoComplete() {
